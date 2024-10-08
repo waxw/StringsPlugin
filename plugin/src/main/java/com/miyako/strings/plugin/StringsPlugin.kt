132f9dba6837c7c747ee3416917002dc455380f7
miyako <weilanxiao@163.com>
2024-10-08 15:37:29 +0800

    plugin module add handle strings task

--- plugin/src/main/java/com/miyako/strings/plugin/StringsPlugin.kt
+++ plugin/src/main/java/com/miyako/strings/plugin/StringsPlugin.kt
@@ -2,6 +2,7 @@ package com.miyako.strings.plugin
 
 import com.miyako.strings.plugin.task.CountStringsTask
 import com.miyako.strings.plugin.task.DeleteStringsTask
+import com.miyako.strings.plugin.task.HandleStringsTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,5 +10,6 @@ class StringsPlugin: Plugin<Project> {
     override fun apply(target: Project) {
         target.tasks.register("countStrings", CountStringsTask::class.java)
         target.tasks.register("deleteStrings", DeleteStringsTask::class.java)
+        target.tasks.register("handleStrings", HandleStringsTask::class.java)
     }
 }
