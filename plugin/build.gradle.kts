f1ece5b99058056ee103b348e32981b75b48f019
miyako <weilanxiao@163.com>
2025-07-30 17:46:14 +0800

    add publish maven central

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
-    `maven-publish`
     `java-gradle-plugin`
     // 这里需要指定 version，否则会提示找不到插件实现类
     kotlin("jvm") version libs.plugins.kotlin.jvm.get().version.displayName
+    // maven publishing 第三方插件，已包含 `maven-publish`
+    alias(libs.plugins.maven.publish.plugin)
 }
+
+val pluginProjetGroup = "com.miyako.strings"
+val publishGroupId = "io.github.waxw"
+val publishVersion = "1.0.0"
+val publishArtifactId = "strings-plugin"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -22,8 +28,43 @@ gradlePlugin {
     }
 }
 
-group = "com.miyako.strings"
+// gradle 本地 group，不影响
+group = pluginProjetGroup
 version = libs.plugins.strings.plugin.get().version
+
+mavenPublishing {
+    coordinates(publishGroupId, publishArtifactId, publishVersion)
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name.set("Strings Plugin")
+        description.set("A plugin that simplifies Android string management.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/waxw/StringsPlugin")
+
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("miyako")
+                name.set("miyako")
+                email.set("weilanxiaogz@outlook.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/waxw/StringsPlugin")
+            connection.set("scm:git:https://github.com/waxw/StringsPlugin.git")
+            developerConnection.set("scm:git:ssh://git@github.com:waxw/StringsPlugin.git")
+        }
+    }
+}
 
 afterEvaluate {
     publishing {
