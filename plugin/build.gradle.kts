cc33ba3c2923c763b6aa3fedb388ff92e370512b
Wei LanXiao <59554769+weilanxiao2020@users.noreply.github.com>
2025-08-01 15:47:17 +0800

    Publish maven central (#2)
    * update .gitignore
    
    * add publish maven central
    
    * disable the gradle plugin marker
    
    ---------
    
    Co-authored-by: miyako <weilanxiao@163.com>

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    `maven-publish`
     `java-gradle-plugin`
     // 这里需要指定 version，否则会提示找不到插件实现类
     kotlin("jvm") version libs.plugins.kotlin.jvm.get().version.displayName
+    // maven publishing 第三方插件，已包含 `maven-publish`
+    alias(libs.plugins.maven.publish.plugin)
 }
 
 java {
@@ -22,8 +23,43 @@ gradlePlugin {
     }
 }
 
+// gradle 本地 group，不影响
 group = "com.miyako.strings"
 version = libs.plugins.strings.plugin.get().version
+
+mavenPublishing {
+    coordinates("io.github.waxw", "strings-plugin", "1.0.1")
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name.set("Strings Plugin")
+        description.set("A plugin that simplifies Android string management.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/waxw/StringsPlugin")
+
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("miyako")
+                name.set("miyako")
+                email.set("weilanxiaogz@outlook.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/waxw/StringsPlugin")
+            connection.set("scm:git:https://github.com/waxw/StringsPlugin.git")
+            developerConnection.set("scm:git:ssh://git@github.com:waxw/StringsPlugin.git")
+        }
+    }
+}
 
 afterEvaluate {
     publishing {
@@ -31,6 +67,12 @@ afterEvaluate {
             maven {
                 setUrl("../local-repo/") // 发布到根项目的 local-repo 路径下
             }
+        }
+    }
+    // 禁用 plugin marker 的生成
+    tasks.withType<PublishToMavenRepository>().configureEach {
+        if (name.contains("PluginMarker")) {
+            enabled = false
         }
     }
 }
